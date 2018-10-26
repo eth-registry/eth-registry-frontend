29458024c10bd3d7981674348a5f7c043c5fd5c3
Alexander Mangel <cygnusfear@gmail.com>
2018-10-26 15:11:50 +0200

    avatar from registry

---
+++ src/components/AccountManager.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Avatar from "@material-ui/core/Avatar";
+import EmptyAvatar from "../assets/empty_avatar.png";
+import "../css/menu.css";
+
+export default class AccountManager extends Component {
+    state = {
+        anchorEl: null,
+    };
+
+    componentWillMount() {
+        if (
+            typeof window.web3 !== "undefined" &&
+            typeof window.web3.currentProvider !== "undefined"
+        ) {
+            this.setState({ web3Provider: window.web3.currentProvider }, () =>
+                this.getProfile(),
+            );
+        }
+    }
+
+    getProfile = async () => {
+        console.log(this);
+        if (window.web3) console.log("getting");
+        const address = window.web3.eth.accounts[0];
+        console.log(address);
+        this.setState({ name: address });
+        let profile = await this.props.ethregistry.getAddressData(address);
+        if (profile) {
+            console.log(profile);
+            this.setState({
+                profile: profile.data.metadata,
+                name: profile.data.metadata.name,
+                avatar: profile.data.metadata.logo,
+            });
+        }
+
+        try {
+        } catch (e) {}
+    };
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    render() {
+        const { anchorEl } = this.state;
+        return (
+            <a
+                className="HeaderNavlink px-2 Account"
+                data-hotkey="g n"
+                aria-owns={anchorEl ? "simple-menu" : null}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+            >
+                <ul
+                    className="user-nav d-flex flex-items-center list-style-none"
+                    id="user-links"
+                >
+                    <li
+                        className={
+                            "name" + (this.state.profile ? " registered" : "")
+                        }
+                    >
+                        {this.state.name}
+                    </li>
+                    <li>
+                        <Avatar
+                            src={this.state.avatar || EmptyAvatar}
+                            style={{
+                                width: 32,
+                                height: 32,
+                                // border: "4px solid black",
+                            }}
+                        />
+                    </li>
+                </ul>
+            </a>
+        );
+    }
+}
